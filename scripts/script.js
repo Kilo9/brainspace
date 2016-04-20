fdd0cb5c468e168979f77076227ed6dba199cb48
Jeremy Bassi <bassij@thingee.com>
2016-04-19 20:34:52 -0400

    get a prototype working

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,28 @@
-$("#new").click(function() {
-  $("section").append("<div class='drag resize'></div>");
-  $('.drag').draggable();
-  $('.resize').resizeable();
+$(function () {
+  function checkStorage(callback)
+  {
+    if (typeof(Storage) !== "undefined") {
+      callback();
+    } else {
+      alert("sorry your browser/settings do not support saving");
+    }
+  }
+
+  $("a#new").click(function() {
+    $("section").append("<div class='drag resize'><textarea>type here...</textarea></div>");
+    $(".drag").draggable();
+    $(".resize").resizable();
+    return false;
+  });
+
+  $("a#save").click(function () {
+      checkStorage(function () {localStorage.setItem("blobs", $("section").html());});
+  });
+
+  $("a#load").click(function () {
+    checkStorage(function () {$("section").append(localStorage.getItem("blobs"));});
+  });
+
+
+
 });
